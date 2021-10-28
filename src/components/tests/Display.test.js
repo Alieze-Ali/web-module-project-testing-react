2e2b8916490e1c6eff94448892b436c421bf6515
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-10-27 21:31:57 -0700

    completed Display, MVP

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,10 +1,85 @@
+//imports
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Display from './../Display';
+
+import mockFetchShow from '../../api/fetchShow';
+jest.mock('../../api/fetchShow');
 
 
 
+//test data
+const testShow = {
+    //add in approprate test data structure here.
+    image: null,
+    name: "show",
+    summary: "This is the summary",
+    seasons: [
+        {
+            episodes: [],
+            id: 1, 
+            name: "season 1"
+        },
+        {
+            episodes: [],
+            id: 2, 
+            name: "season 1"
+        },
+        {
+            episodes: [],
+            id: 3, 
+            name: "season 1"
+        }
+    ]
 
 
+}
 
 
+//test the Display component renders
+test('renders without props', () => {
+    render(<Display/>);
+});
+
+//testing fetch button calls the show component 'that's so fetch!'
+test('fetch button displays the show component', async () => {
+    render(<Display/>)
+    mockFetchShow.mockResolvedValueOnce(testShow);
+
+    const button = screen.getByRole('button', /Press to Get Show Data/i);
+    userEvent.click(button);
+
+    const showID = await screen.findByTestId('show-container')
+
+    expect(showID).toBeInTheDocument();
+    expect(showID).toHaveTextContent('show');
+    // somethings wrong here???
+});
+
+//testing when fetch button is pressed, the amoutn of select options rendered is equal to the amount of seasons in testData
+test('fetch button select options equal seasons amount', async () => {
+    render(<Display />);
+    mockFetchShow.mockResolvedValueOnce(testShow)
+
+    const button = screen.getByRole('button', 'Press to Get Show Data/i');
+    userEvent.click(button);
+    const seasonsOptions = await screen.findAllByTestId("season-option");
+
+    expect(seasonsOptions).toHaveLength(3);
+})
+
+//testing when fetch button is pressed, the function is called
+test('when fetch is pressed, function is called' , async () => {
+    const mockDisplayFunc = jest.fn();
+    render(<Display displayFunc={mockDisplayFunc}/>); //mock function
+    mockFetchShow.mockResolvedValueOnce(testShow); //mock api call
+
+    const button = screen.getByRole('button', /Press to Get Show Data/i);
+    userEvent.click(button);
+
+    await waitFor(() => expect(mockDisplayFunc).toHaveBeenCalledTimes(1));
+})
 
 
 
